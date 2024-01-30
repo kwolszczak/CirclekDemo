5746c7d69b9973527b9c755df5553ff8a3068445
Kamil Wolszczak <kwolszczak@sii.pl>
2024-01-30 12:28:31 +0100

    clean code and BasePage with driver wrapper

--- src/main/java/dev/kwolszczak/pages/HomePage.java
+++ src/main/java/dev/kwolszczak/pages/HomePage.java
@@ -2,6 +2,7 @@ package dev.kwolszczak.pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class HomePage extends BasePage {
@@ -14,7 +15,11 @@ public class HomePage extends BasePage {
     }
 
     public String getLoginMessage() {
-        wait.until(ExpectedConditions.invisibilityOfAllElements(driver.findElements(spinner)));
+        wait.until(spinnerInvisibility());
         return getText(message);
+    }
+
+    private ExpectedCondition<Boolean> spinnerInvisibility() {
+        return ExpectedConditions.invisibilityOfAllElements(driver.findElements(spinner));
     }
 }
