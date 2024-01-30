401ffd95dffa92f2dbcb05509b886e7256015949
Kamil Wolszczak <kwolszczak@sii.pl>
2024-01-30 12:02:21 +0100

    e2e test for successful login

--- src/test/java/demoTest/LoginTest.java
+++ src/test/java/demoTest/LoginTest.java
@@ -1,5 +1,6 @@
 package demoTest;
 
+import dev.kwolszczak.pages.HomePage;
 import dev.kwolszczak.pages.LoginPage;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Assertions;
@@ -9,18 +10,22 @@ import org.junit.jupiter.api.Test;
 class LoginTest extends BaseTest {
 
     @Test
-    public void verifyLoginWithCorrectUsernameAndPassword() {
+    public void verifyLoginWithCorrectUsernameAndPassword() throws InterruptedException {
         var expectedTitle = "Login System";
         String password = "pass123";
         String username = "test01@example.com";
+        String loginSuccessful = "Login successful";
 
         driver.get(" https://circlek-public.github.io/cki-service-recruitment-task/");
         String actualTitle = driver.getTitle();
         log.info("'Opened login page, Title: '{}'", actualTitle);
 
-        Assertions.assertEquals(expectedTitle, actualTitle);
-
         LoginPage loginPage = new LoginPage(driver);
         loginPage.login(username, password);
+
+        HomePage homePage = new HomePage(driver);
+        var actualLoginMessage = homePage.getLoginMessage();
+
+        Assertions.assertEquals(loginSuccessful, actualLoginMessage);
     }
 }
