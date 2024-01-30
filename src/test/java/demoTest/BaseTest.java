c231ab206c18897b481039d3c3cbf861c01b6357
Kamil Wolszczak <kwolszczak@sii.pl>
2024-01-30 20:24:12 +0100

    generic constructor (at) for Page.classes

--- src/test/java/demoTest/BaseTest.java
+++ src/test/java/demoTest/BaseTest.java
@@ -12,14 +12,14 @@ import java.io.IOException;
 import java.util.Properties;
 
 public class BaseTest {
+    private final String path = "testdata.properties";
     protected WebDriver driver;
     protected Properties properties;
 
     @BeforeEach
     public void setUp() throws IOException {
         driver = new ChromeDriver();
-        properties = DataUtil.getProperties("testdata.properties");
-
+        properties = DataUtil.getProperties(path);
     }
 
     @AfterEach
